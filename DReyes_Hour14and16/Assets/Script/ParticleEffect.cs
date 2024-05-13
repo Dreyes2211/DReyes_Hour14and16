1edbbb270bf6bf0ca1ac1ef4fb86f18ec2216cea
Dreyes2211 <dannyre.1111@gmail.com>
2024-05-12 20:55:15 -0700

    HOUR 16 WORK
    work for  hour 16

---
+++ DReyes_Hour14and16/Assets/Script/ParticleEffect.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleEffect : MonoBehaviour
+{
+// changed the shape of the emittor so the flow would change//
+
+// changed the velocity over lifetime to make more lighting like//
+
+//changed size by speed so it tappers of at the end//
+
+//added ribbon trail to give it lighting effect//
+
+//changed color to make it look like a lighting bolt// 
+}
